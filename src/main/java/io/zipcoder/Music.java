b527caade8589fcc627e42bff2a9c83910fca564
Zanetta Norris <znorris@zipcodes-MacBook-Pro-2.local>
2020-03-01 22:59:35 -0500

    added code to pass two tests

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -1,14 +1,27 @@
 package io.zipcoder;
 
+import java.util.Arrays;
+
 public class Music {
 
     private String[] playList;
+
+
 
     public Music(String[] playList){
         this.playList = playList;
     }
 
     public Integer selection(Integer startIndex, String selection){
-        return null;
+       Integer steps = 0;
+        Integer startSongIndex = Arrays.asList(playList).indexOf(startIndex)+1;
+        Integer chosenSongIndex = Arrays.asList(playList).indexOf(selection)-1;
+        if (chosenSongIndex > startSongIndex){
+            steps = chosenSongIndex - startSongIndex;}
+        else if (chosenSongIndex < startSongIndex){
+            steps = startSongIndex - chosenSongIndex;}
+
+return steps;
     }
 }
+
